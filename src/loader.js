e45a4ef16e6ffd47ae0f1ab970757d882b72854c
Niall Molloy <niall.molloy@engineering.digital.dwp.gov.uk>
2025-12-01 15:49:40 +0000

    perf: cache resolved absolute template paths, speed up isRelative check

--- src/loader.js
+++ src/loader.js
@@ -4,11 +4,42 @@ const path = require('node:path');
 const { EmitterObj } = require('./object');
 
 module.exports = class Loader extends EmitterObj {
+  #resolveCache = new Map();
+
+  /**
+   * Resolve relative nunjucks template path to absolute path.
+   * @param {string} from
+   * @param {string} to
+   * @returns {string} absolute path
+   */
   resolve (from, to) {
-    return path.resolve(path.dirname(from), to);
+    const key = from + to;
+    let resolved = this.#resolveCache.get(key);
+    if (resolved) {
+      return resolved;
+    }
+    resolved = path.resolve(path.dirname(from), to);
+    this.#resolveCache.set(key, resolved);
+    return resolved;
   }
 
+  /**
+   * Determine with a nunjucks template path is relative; does it start with
+   * `"./"` or `"../"`.
+   * @param {string} filename
+   * @returns {boolean}
+   */
   isRelative (filename) {
-    return (filename.indexOf('./') === 0 || filename.indexOf('../') === 0);
+    const char1 = filename.charCodeAt(0);
+    const char2 = filename.charCodeAt(1);
+
+    // 46 = "."; 47 = "/"
+    if (char1 === 46 && char2 === 47) {
+      return true;
+    }
+    if (char1 === 46 && char2 === 46 && filename.charCodeAt(2) === 47) {
+      return true;
+    }
+    return false;
   }
 };
