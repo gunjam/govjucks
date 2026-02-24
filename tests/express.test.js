61060cf6adf43af88803c331eede9d84ee341569
Niall Molloy <niall.molloy@engineering.digital.dwp.gov.uk>
2026-02-24 15:16:15 +0000

    test: replace supertest with light-my-request

--- tests/express.test.js
+++ tests/express.test.js
@@ -4,7 +4,7 @@ const assert = require('node:assert/strict');
 const path = require('node:path');
 const { beforeEach, describe, it } = require('node:test');
 const express = require('express');
-const request = require('supertest');
+const inject = require('light-my-request');
 const govjucks = require('../index');
 
 const VIEWS = path.join(__dirname, '../samples/express/views');
@@ -27,24 +27,22 @@ describe('express', () => {
     assert.deepEqual(app.settings.nunjucksEnv, env);
   });
 
-  it('should render a view with extension', (t, done) => {
+  it('should render a view with extension', async () => {
     app.get('/', (req, res) => {
       res.render('about.html');
     });
-    request(app)
-      .get('/')
-      .expect(/This is just the about page/)
-      .end(done);
+
+    const res = await inject(app, { path: '/' });
+    assert.match(res.payload, /This is just the about page/);
   });
 
-  it('should render a view without extension', (t, done) => {
+  it('should render a view without extension', async () => {
     app.get('/', (req, res) => {
       res.render('about');
     });
     app.set('view engine', 'html');
-    request(app)
-      .get('/')
-      .expect(/This is just the about page/)
-      .end(done);
+
+    const res = await inject(app, { path: '/' });
+    assert.match(res.payload, /This is just the about page/);
   });
 });
